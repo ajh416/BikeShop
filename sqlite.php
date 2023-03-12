23ceaa13fed512f7275d22d1188fe503cc6d1bb1
Adam Henry <114379115+ajh416@users.noreply.github.com>
2023-03-12 14:58:46 -0700

    update sqlite.php
    add headers and proper displaying of result set.

--- sqlite.php
+++ sqlite.php
@@ -11,14 +11,22 @@ if((isset($_POST['query'])) && !empty($_POST['query']))
 	$db = new SQLite3('mydb.sq3');
 	$query = $_POST['query'];
 	$result = $db->query($query);
+	$num = $result->numColumns();
+	$arr = array();
+	for ($i = 0; $i < $num; $i++) {
+		array_push($arr, $result->columnName($i));
+	}
+	foreach ($arr as $val) {
+		echo "$val ";
+	}
+	echo '<br/>';
 	while ($row = $result->fetchArray(SQLITE3_ASSOC)){
 		foreach ($row as $rvalue) {
-			//echo $row['eid'] . ': $' . $row['price'] . '<br/>';
 			echo $rvalue . " ";
 		}
 		echo '<br/>';
 	}
 	unset($db);
-	echo "Your query was: $query";
+	echo "<br/> The last query was: $query";
 }
 ?>
